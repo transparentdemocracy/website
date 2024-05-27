523071d87a5cff3bb3d9e444a3628d8433cb6a07
idotj <info@idotj.com>
2024-05-27 19:43:06 +0200

    First working version

--- src/app/language-selector/language-selector.component.ts
+++ src/app/language-selector/language-selector.component.ts
@@ -1,16 +1,30 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import {LanguageService} from "../services/language.service";
+import { Component, OnInit } from '@angular/core';
+import { LanguageService } from '../services/language.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'language-selector',
   templateUrl: './language-selector.component.html',
   standalone: true,
-  styleUrl: './language-selector.component.sass'
+  styleUrl: './language-selector.component.sass',
 })
-export class LanguageSelector {
-  constructor(private languageService: LanguageService) { }
+export class LanguageSelector implements OnInit {
+  language: string = '';
 
-  triggerLanguageChange(selectedLanguage: string) {
-    this.languageService.setLanguage(selectedLanguage)
+  ngOnInit(): void {
+    this.language = localStorage.getItem('lang') ?? 'nl';
+  }
+
+  constructor(
+    private languageService: LanguageService,
+    private translate: TranslateService
+  ) {}
+
+  triggerLanguageChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedLanguage = selectElement.value;
+    this.languageService.setLanguage(selectedLanguage);
+    localStorage.setItem('lang', selectedLanguage);
+    this.translate.use(selectedLanguage);
   }
 }
