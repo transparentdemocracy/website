e1fbcb8217541df3b9ec835f8976fa8a72b73a33
idotj <info@idotj.com>
2024-06-01 15:52:25 +0200

    Mobile dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,5 @@ export class AppComponent {
   ) {
     this.languageConfigurationService.initLanguageSetup();
   }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageSelector } from '../language/language-selector/language-selector.component';
 import { NavigationComponent } from '../navigation/navigation.component';
@@ -10,4 +10,22 @@ import { NavigationComponent } from '../navigation/navigation.component';
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isScrolledToBottom: boolean = false;
+  isMenuMobileOpen: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition =
+      window.scrollY ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    this.isScrolledToBottom = scrollPosition > 0;
+  }
+
+  public toogleMenuMobile() {
+    this.isMenuMobileOpen = !this.isMenuMobileOpen;
+  }
+}
