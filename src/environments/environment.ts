3081f57e16d057090707d9285abcda42564dfa01
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-11-02 18:58:08 +0100

    disable maintenance mode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   searchMotionsUrl: 'https://3qfjsuzusi5gdx3ddo3z6bfz6m0sevhb.lambda-url.eu-west-1.on.aws/',
   searchPlenariesUrl: 'https://57wkgejqgpfvl76smdy4xoahdm0ncinc.lambda-url.eu-west-1.on.aws/',
   getMotionUrl: 'https://sl7hjec5i33wxztyzodohkaiiu0ewkna.lambda-url.eu-west-1.on.aws/',
-  maintenanceModeEnabled: true
+  maintenanceModeEnabled: false
 };
