e74efb2e39df560f30f1d4a1582d7f76544d0701
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-11-03 12:12:20 +0100

    Make sure prod uses prod lambdas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  searchMotionsUrl: 'https://4o7ibsjhpuik4w5f65cld5bwya0swnax.lambda-url.eu-west-1.on.aws/',
-  searchPlenariesUrl: 'https://lriab2tv73b4i4aau6kpmoj7240lvarc.lambda-url.eu-west-1.on.aws/',
-  getMotionUrl: 'https://iphipqnevfb76rdjkiyvcw4p7q0obavq.lambda-url.eu-west-1.on.aws/',
+  searchMotionsUrl: 'https://q7stmtacy62cju3z6s5grojtfe0hevkb.lambda-url.eu-west-1.on.aws/',
+  searchPlenariesUrl: 'https://4uvvhclrigrf2naaecxc4goa6i0mkyxt.lambda-url.eu-west-1.on.aws/',
+  getMotionUrl: 'https://peo2skbrwz6rbtunqpjix3rmgi0hbrwi.lambda-url.eu-west-1.on.aws/',
   maintenanceModeEnabled: false
 };
