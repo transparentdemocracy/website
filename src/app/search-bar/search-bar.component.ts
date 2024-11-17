0e49322ee171339cd7f6c71f6018b6ba9a3c3ded
Karel Vervaeke <karel@vervaeke.info>
2024-11-17 18:42:21 +0100

    Added bootstrap because layout is easier to manage with a grid system

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -3,6 +3,8 @@ import {FormsModule, NG_VALUE_ACCESSOR} from "@angular/forms";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {TranslateModule} from "@ngx-translate/core";
 import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'search-bar',
@@ -10,7 +12,9 @@ import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
   imports: [
     FormsModule,
     FaIconComponent,
-    TranslateModule
+    TranslateModule,
+    MatDatepickerModule,
+    MatInputModule
   ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.sass',
