0a6c44d025f24b312208567f77b8d2e312841acb
Karel Vervaeke <karel@vervaeke.info>
2025-03-16 21:37:02 +0100

    set up 3rd profile.
    We now have prod & develop using the corresponding lambdas and 'local' using the dev lambdas via localhost proxy

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  searchMotionsUrl: 'https://4o7ibsjhpuik4w5f65cld5bwya0swnax.lambda-url.eu-west-1.on.aws/',
+  searchPlenariesUrl: 'https://lriab2tv73b4i4aau6kpmoj7240lvarc.lambda-url.eu-west-1.on.aws/',
+  getMotionUrl: 'https://iphipqnevfb76rdjkiyvcw4p7q0obavq.lambda-url.eu-west-1.on.aws/',
+  maintenanceModeEnabled: false,
+  showLogin: false,
+  firebaseConfig: null,
+};
