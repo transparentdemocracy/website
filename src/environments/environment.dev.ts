41a32a28796f8f12ead1d8606170b216e8d499a5
Karel Vervaeke <karel@vervaeke.info>
2025-03-16 22:11:40 +0100

    copied firebase config to dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,5 +5,13 @@ export const environment = {
   getMotionUrl: 'https://iphipqnevfb76rdjkiyvcw4p7q0obavq.lambda-url.eu-west-1.on.aws/',
   maintenanceModeEnabled: false,
   showLogin: false,
-  firebaseConfig: null,
+  firebaseConfig: {
+    apiKey: "AIzaSyAXt3nTsg0w4CG26Emjn_f2tUaRr36eU90",
+    authDomain: "wddp-d1431.firebaseapp.com",
+    projectId: "wddp-d1431",
+    storageBucket: "wddp-d1431.firebasestorage.app",
+    messagingSenderId: "758673969029",
+    appId: "1:758673969029:web:22ef941742484ceddabeec",
+    measurementId: "G-JR9KSCE8XV"
+  },
 };
